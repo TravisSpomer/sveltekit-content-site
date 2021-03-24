ee444de94692faf73e4ed0d25f2a28f02ea512f1
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-03-24 09:26:47 -0700

    Initial commit

---
+++ src/routes/content/[filename].svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+	import MarkdownIt from "markdown-it"
+
+	const md = new MarkdownIt({
+		html: true,
+		linkify: true,
+		quotes: "“”‘’",
+		typographer: true,
+	})
+
+	export async function load({ page, fetch, session, context })
+	{
+		const html = md.render(`Hello this is **${page.params.filename}** rendered using Markdown`)
+		return { props: { filename: page.params.filename, html: html } }
+	}
+</script>
+
+<script lang="ts">
+	export let html: string
+</script>
+
+<main>
+	{@html html}
+</main>
---
+++ src/routes/page-two.svelte
@@ -0,0 +1,13 @@
+<script>
+
+import Counter from "$lib/Counter.svelte";
+
+</script>
+<main>
+	<p>
+		Creating a second page is as easy as 1-2-...
+	</p>
+	<p>
+		Now let's go crazy and do <a href="/content/page-three">page 3</a> as a dynamic route!
+	</p>
+</main>
